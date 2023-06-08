75a9b71b6d137c72de2fc877fb88c0925cc134bf
Atul <atul.vishwakarma@tecorb.co>
2023-06-08 17:23:48 +0530

    Initaial commit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # SnapShotMacOS
 
 A description of this package.
+
+## Installation 
+To Install this package , import 'https://github.com/atultecorb/SnapShotMacOS.git' in SPM
+
+## Usage Example
--- Sources/SnapShotMacOS/SnapShotMacOS.swift
+++ Sources/SnapShotMacOS/SnapShotMacOS.swift
@@ -1,6 +1,48 @@
-public struct SnapShotMacOS {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import AVKit
+import AVFoundation
 
-    public init() {
+
+public class ScreenCapture {
+    static let shareInstance = ScreenCapture()
+    
+    init() {}
+    
+    public var captureImage = NSImage()
+    
+    public init(captureImage: NSImage = NSImage()) {
+        self.captureImage = captureImage
+    }
+    
+   public func TakeScreensShots(folderName: String){
+        
+        var displayCount: UInt32 = 0;
+        var result = CGGetActiveDisplayList(0, nil, &displayCount)
+        if (result != CGError.success) {
+            print("error: \(result)")
+            return
+        }
+        let allocated = Int(displayCount)
+        let activeDisplays = UnsafeMutablePointer<CGDirectDisplayID>.allocate(capacity: allocated)
+        result = CGGetActiveDisplayList(displayCount, activeDisplays, &displayCount)
+        
+        if (result != CGError.success) {
+            print("error: \(result)")
+            return
+        }
+           
+        for i in 1...displayCount {
+            let unixTimestamp = CreateTimeStamp()
+            let fileUrl = URL(fileURLWithPath: folderName + "\(unixTimestamp)" + "_" + "\(i)" + ".jpg", isDirectory: true)
+            let screenShot:CGImage = CGDisplayCreateImage(activeDisplays[Int(i-1)])!
+            let bitmapRep = NSBitmapImageRep(cgImage: screenShot)
+            let jpegData = bitmapRep.representation(using: NSBitmapImageRep.FileType.jpeg, properties: [:])!
+            self.captureImage = NSImage(data: jpegData)!
+        }
+    }
+    
+    func CreateTimeStamp() -> Int32
+    {
+        return Int32(Date().timeIntervalSince1970)
     }
 }
